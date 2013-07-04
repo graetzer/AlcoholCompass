d61a01224057ca0da28246da6d42d05d4daa42f5
Frizzware <2aletmann>
2013-07-05 00:01:02 +0200

    Alles

---
+++ app/AlcoholCompass/src/com/alcoholcompass/PlacesListAdapter.java
@@ -0,0 +1,52 @@
+package com.alcoholcompass;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.alcoholcompass.data.Place;
+
+public class PlacesListAdapter extends BaseAdapter {
+
+	private LayoutInflater inflater;
+	private ArrayList<Place> items;
+	
+	public PlacesListAdapter(Context ctx, ArrayList<Place> items){
+		inflater = LayoutInflater.from(ctx);
+		this.items = items;
+	}
+	
+	@Override
+	public int getCount() {
+		return items.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return items.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		return arg0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		View rowView = inflater.inflate(R.layout.row_places, null, false);
+		
+		TextView name = (TextView) rowView.findViewById(R.id.textViewRowPlacesName);
+		TextView distance = (TextView) rowView.findViewById(R.id.textViewRowPlacesDistance);
+		
+		name.setText(items.get(arg0).getName());
+		distance.setText(items.get(arg0).getDistance() + "m");
+		
+		return rowView;
+	}
+
+}
