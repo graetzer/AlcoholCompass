64fc6cd753750dd84a4e8be0105b47264e627a24
Simon Grätzer <simon@graetzer.org>
2013-07-04 19:52:15 +0200

    fragment_navigation

--- app/AlcoholCompass/src/com/alcoholcompass/MainActivity.java
+++ app/AlcoholCompass/src/com/alcoholcompass/MainActivity.java
@@ -14,17 +14,7 @@ import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
-
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	TabsAdapter mPagerAdapter;
-
+	private TabsAdapter mPagerAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +27,24 @@ public class MainActivity extends FragmentActivity {
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
-		mPagerAdapter = new TabsAdapter(this, (ViewPager)findViewById(R.id.pager));
-		
-		mPagerAdapter.addTab(bar.newTab().setText(R.string.fragment_navigation_title), 
+		mPagerAdapter = new TabsAdapter(this,
+				(ViewPager) findViewById(R.id.pager));
+
+		mPagerAdapter.addTab(
+				bar.newTab().setText(R.string.fragment_navigation_title),
 				NavigatorFragment.class, null);
-		
-	}
+
+        if (savedInstanceState != null) {
+            bar.setSelectedNavigationItem(savedInstanceState.getInt("tab"));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("tab", getActionBar()
+                .getSelectedNavigationIndex());
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,95 +54,96 @@ public class MainActivity extends FragmentActivity {
 	}
 
 	/**
-     * This is a helper class that implements the management of tabs and all
-     * details of connecting a ViewPager with associated TabHost. It relies on a
-     * trick. Normally a tab host has a simple API for supplying a View or
-     * Intent that each tab will show. This is not sufficient for switching
-     * between pages. So instead we make the content part of the tab host 0dp
-     * high (it is not shown) and the TabsAdapter supplies its own dummy view to
-     * show as the tab content. It listens to changes in tabs, and takes care of
-     * switch to the correct paged in the ViewPager whenever the selected tab
-     * changes.
-     */
-    private static class TabsAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener, ActionBar.TabListener {
-        private final Context mContext;
-        private final ActionBar mBar;
-        private final ViewPager mViewPager;
-        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+	 * This is a helper class that implements the management of tabs and all
+	 * details of connecting a ViewPager with associated TabHost. It relies on a
+	 * trick. Normally a tab host has a simple API for supplying a View or
+	 * Intent that each tab will show. This is not sufficient for switching
+	 * between pages. So instead we make the content part of the tab host 0dp
+	 * high (it is not shown) and the TabsAdapter supplies its own dummy view to
+	 * show as the tab content. It listens to changes in tabs, and takes care of
+	 * switch to the correct paged in the ViewPager whenever the selected tab
+	 * changes.
+	 */
+	private static class TabsAdapter extends FragmentPagerAdapter implements
+			ViewPager.OnPageChangeListener, ActionBar.TabListener {
+		private final Context mContext;
+		private final ActionBar mBar;
+		private final ViewPager mViewPager;
+		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
 
-        static final class TabInfo {
-            private final Class<?> clss;
-            private final Bundle args;
+		static final class TabInfo {
+			private final Class<?> clss;
+			private final Bundle args;
 
-            TabInfo(Class<?> _class, Bundle _args) {
-                clss = _class;
-                args = _args;
-            }
-        }
+			TabInfo(Class<?> _class, Bundle _args) {
+				clss = _class;
+				args = _args;
+			}
+		}
 
-        public TabsAdapter(FragmentActivity activity, ViewPager pager) {
-            super(activity.getSupportFragmentManager());
-            mContext = activity;
-            mBar = activity.getActionBar();
-            mViewPager = pager;
-            mViewPager.setAdapter(this);
-            mViewPager.setOnPageChangeListener(this);
-        }
+		public TabsAdapter(FragmentActivity activity, ViewPager pager) {
+			super(activity.getSupportFragmentManager());
+			mContext = activity;
+			mBar = activity.getActionBar();
+			mViewPager = pager;
+			mViewPager.setAdapter(this);
+			mViewPager.setOnPageChangeListener(this);
+		}
 
-        public void addTab(ActionBar.Tab tab, Class<? extends Fragment> clss,
-                           Bundle args) {
-            TabInfo info = new TabInfo(clss, args);
-            tab.setTag(info);
-            tab.setTabListener(this);
-            mTabs.add(info);
-            mBar.addTab(tab);
-            notifyDataSetChanged();
-        }
+		public void addTab(ActionBar.Tab tab, Class<? extends Fragment> clss,
+				Bundle args) {
+			TabInfo info = new TabInfo(clss, args);
+			tab.setTag(info);
+			tab.setTabListener(this);
+			mTabs.add(info);
+			mBar.addTab(tab);
+			notifyDataSetChanged();
+		}
 
-        @Override
-        public int getCount() {
-            return mTabs.size();
-        }
+		@Override
+		public int getCount() {
+			return mTabs.size();
+		}
 
-        @Override
-        public Fragment getItem(int position) {
-            TabInfo info = mTabs.get(position);
-            return Fragment.instantiate(mContext, info.clss.getName(),
-                    info.args);
-        }
+		@Override
+		public Fragment getItem(int position) {
+			TabInfo info = mTabs.get(position);
+			return Fragment.instantiate(mContext, info.clss.getName(),
+					info.args);
+		}
 
-        @Override
-        public void onPageScrolled(int position, float positionOffset,
-                                   int positionOffsetPixels) {
-        }
+		@Override
+		public void onPageScrolled(int position, float positionOffset,
+				int positionOffsetPixels) {
+		}
 
-        @Override
-        public void onPageSelected(int position) {
-            mBar.setSelectedNavigationItem(position);
-        }
+		@Override
+		public void onPageSelected(int position) {
+			mBar.setSelectedNavigationItem(position);
+		}
 
-        @Override
-        public void onPageScrollStateChanged(int state) {
-        }
+		@Override
+		public void onPageScrollStateChanged(int state) {
+		}
 
-        @Override
-        public void onTabSelected(Tab tab, FragmentTransaction ft) {
-            Object tag = tab.getTag();
-            for (int i = 0; i < mTabs.size(); i++) {
-                if (mTabs.get(i) == tag) {
-                    mViewPager.setCurrentItem(i);
-                }
-            }
-        }
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			Object tag = tab.getTag();
+			for (int i = 0; i < mTabs.size(); i++) {
+				if (mTabs.get(i) == tag) {
+					mViewPager.setCurrentItem(i);
+				}
+			}
+		}
 
-        @Override
-        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 
-        }
+		}
 
-        @Override
-        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
 
-        }
-    }
+		}
+	}
 }
