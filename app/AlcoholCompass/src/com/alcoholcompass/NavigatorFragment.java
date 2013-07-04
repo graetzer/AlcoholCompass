e56676ea3741ff0dd7c08409c88add2fcd3c3ac9
Frizzware <2aletmann>
2013-07-04 19:37:53 +0200

    Navigation Fragment

--- app/AlcoholCompass/src/com/alcoholcompass/NavigatorFragment.java
+++ app/AlcoholCompass/src/com/alcoholcompass/NavigatorFragment.java
@@ -5,10 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class NavigatorFragment extends Fragment{
+	
+	private ImageView imageViewArrow;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.fragment_navigation, container, false);
+		
+		//Views zuweisen
+		imageViewArrow = (ImageView) view.findViewById(R.id.imageViewArrow);
+		return view;
 	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
 }
