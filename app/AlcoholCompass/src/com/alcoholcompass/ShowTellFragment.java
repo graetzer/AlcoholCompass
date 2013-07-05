c3d56b28e7a0366f98bfab363624ec1e6e629d01
Frizzware <2aletmann>
2013-07-05 03:03:26 +0200

    All about Fragments

--- app/AlcoholCompass/src/com/alcoholcompass/ShowTellFragment.java
+++ app/AlcoholCompass/src/com/alcoholcompass/ShowTellFragment.java
@@ -1,16 +1,27 @@
 package com.alcoholcompass;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ShowTellFragment extends Fragment {
 
+	Typeface cola;
+	TextView title;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_showtell, container, false);
+		
+		cola = Typeface.createFromAsset(getActivity().getAssets(), "fonts/cola.ttf");
+		title = (TextView) view.findViewById(R.id.textViewAlcoholCompass);
+		title.setTypeface(cola);
+		
+		
 		return view;
 	}
 
