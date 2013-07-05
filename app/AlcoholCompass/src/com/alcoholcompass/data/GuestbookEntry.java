5daace60d8a9be1cceca65a5a894eb7b96ddfcc4
Simon Grätzer <simon@graetzer.org>
2013-07-05 04:25:30 +0200

    Upload service

--- app/AlcoholCompass/src/com/alcoholcompass/data/GuestbookEntry.java
+++ app/AlcoholCompass/src/com/alcoholcompass/data/GuestbookEntry.java
@@ -4,4 +4,6 @@ public class GuestbookEntry {
 	public int id;
 	public String user;
 	public long created;
+	public String text;
+	public String imageUrl;
 }
--- app/AlcoholCompass/src/com/alcoholcompass/data/WebService.java
+++ app/AlcoholCompass/src/com/alcoholcompass/data/WebService.java
@@ -1,28 +1,36 @@
 package com.alcoholcompass.data;
 
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.entity.StringEntity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Context;
 import android.location.Location;
 import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 public class WebService {
 	private final static String TAG = "WebService";
 	private final static String Endpoint = "http://31.172.42.83/nearby.json";
+	private final static String EndpointGuestbook = "http://31.172.42.83/guestbooks";
+	private final static String EndpointImages = "http://trash.ctdo.de/bintrash.php";
 	private static AsyncHttpClient client = new AsyncHttpClient();
-	
+
 	public static void loadPlaces(Location loc, final PlacesHandler handler) {
-		if (loc == null || handler == null) throw new InvalidParameterException("No parameter may be null");
-		
+		if (loc == null || handler == null)
+			throw new InvalidParameterException("No parameter may be null");
+
 		RequestParams params = new RequestParams();
 		params.put("latitude", String.valueOf(loc.getLatitude()));
 		params.put("longitude", String.valueOf(loc.getLongitude()));
@@ -33,16 +41,12 @@ public class WebService {
 				try {
 					for (int i = 0; i < array.length(); i++) {
 						JSONObject obj = array.getJSONObject(i);
-						Place p = new Place(
-								obj.optInt("id"),
-								obj.optString("name"),
-								obj.optString("address"),
-								obj.optInt("open_at"),
-								obj.optInt("closed_at"),
-								obj.optDouble("latitude"),
-								obj.optDouble("longitude")
-								);
-						
+						Place p = new Place(obj.optInt("id"), obj
+								.optString("name"), obj.optString("address"),
+								obj.optInt("open_at"), obj.optInt("closed_at"),
+								obj.optDouble("latitude"), obj
+										.optDouble("longitude"));
+
 						JSONArray entries = obj.getJSONArray("guestbooks");
 						ArrayList<GuestbookEntry> list = new ArrayList<GuestbookEntry>();
 						for (int j = 0; j < entries.length(); j++) {
@@ -56,12 +60,13 @@ public class WebService {
 						p.setGuestbookEntries(list);
 						places.add(p);
 					}
-					
+
 				} catch (JSONException e) {
 					Log.e(TAG, "JSON Error", e);
 				}
 				handler.success(places);
 			}
+
 			@Override
 			public void onFailure(Throwable t, String msg) {
 				Log.e(TAG, msg, t);
@@ -69,13 +74,59 @@ public class WebService {
 			}
 		});
 	}
-	
-	public static void sendGuestbookEntry(Place place, String user, String text, byte[]image) {
-		
+
+	public static void sendGuestbookEntry(final Context ctx, final Place place,
+			final String user, final String text, byte[] image) {
+
+		RequestParams params = new RequestParams();
+		ByteArrayInputStream input = new ByteArrayInputStream(image);
+		params.put("action", "upload");
+		params.put("upfile", input, "image.jpeg");
+		params.put("validity", "9");
+
+		client.put(EndpointImages, params, new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String message) {
+				int start = message.indexOf("Fuer Foren etc: <a href=\"");
+				int end = message.indexOf("\">http://trash.ctdo.de/b/", start);				
+				String url = "";
+				if (start != -1 && end != -1)
+					url = message.substring(start, end);
+				
+				uploadStuff(ctx, place, user, text, url);
+			}
+			
+			@Override
+			public void onFailure(Throwable arg0, String arg1) {
+				String url = "";
+				uploadStuff(ctx, place, user, text, url);
+			}
+		});
+
 	}
-	
+
+	private static void uploadStuff(final Context ctx, final Place place,
+			final String user, final String text, String url) {
+		try {
+			String json = "{guestbook:{user: \"" + user
+					+ "\",location_id: " + place.getID() + ", text: \""
+					+ text + "\", url: \""+url+"\"}}";
+			
+			client.post(ctx, EndpointGuestbook, new StringEntity(json),
+					"application/json", new AsyncHttpResponseHandler() {
+						@Override
+						public void onSuccess(String arg1) {
+							Log.i(TAG, "Success");
+						}
+					});
+		} catch (UnsupportedEncodingException e) {
+			Log.e(TAG, "Error sending guestbook entry", e);
+		}
+	}
+
 	public interface PlacesHandler {
 		public void success(List<Place> places);
+
 		public void failure();
 	}
 }
